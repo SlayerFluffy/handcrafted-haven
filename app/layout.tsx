2c6873101085773166ac97dc989a1e5bf0f77fd3
Denton Flake <me@dentonflake.com>
2026-03-11 19:58:20 -0700

    Added basic header and footer

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import "@/app/globals.css"
+import type { Metadata } from "next"
+import { montserrat } from "@/app/lib/fonts"
+import Header from "@/app/components/layout/header"
+import Footer from "@/app/components/layout/footer"
+
+export const metadata: Metadata = {
+  title: "Handcrafted Haven",
+  description: "WDD 430 Team Project"
+}
+
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} min-h-screen antialiased`}>
+        <Header />
+        <main className="min-h-[calc(100vh-128px)]">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+  return (
+    <section className="px-6 py-8">
+      <p>Home Page</p>
+    </section>
+  )
+}
+
+export default Page
