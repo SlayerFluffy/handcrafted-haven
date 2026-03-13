76e37c59d1d3d4b103190b9e9564fed4c131a483
Denton Flake <me@dentonflake.com>
2026-03-13 15:09:33 -0700

    Styled footer

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,9 +1,13 @@
 
 const Footer = () => {
 
+  const year = new Date().getFullYear()
+
   return (
-    <footer className="bg-gray-100 px-6 py-4">
-      <p className="text-gray-800">Basic Footer</p>
+    <footer className="bg-gray-800 px-10 py-8">
+      <p className="text-sm text-gray-400 text-center">
+        &copy; {year} Handcrafted Haven. All rights reserved.
+      </p>
     </footer>
   )
 }
